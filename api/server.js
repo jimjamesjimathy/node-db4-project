37b5f00765cf59a5055e89385f71d028115db263
James Kirkwood <85250373+jimjamesjimathy@users.noreply.github.com>
2022-02-17 16:35:38 -0500

    will work on this later. have to go to work

--- api/server.js
+++ api/server.js
@@ -2,13 +2,13 @@ const express = require('express')
 const recipesRouter = require('./recipes/recipes-router')
 
 const server = express()
-const recRouter = require('./recipes/recipes-router');
+
 server.use(express.json())
 
-server.use('/api/recipes', recRouter)
+server.use('/api/recipes', recipesRouter)
 
 server.use('*', (req, res) => {
     res.json({ api: 'up'})
 })
 
-module.exports = server;
+module.exports = server
